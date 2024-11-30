ab06561bc373d85086ded20af1b2912131d6be0a
SandorMore <msaso0725@gmail.com>
2024-11-30 12:43:36 +0100

    l

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ font = pygame.font.Font('freesansbold.ttf', 20)
 big_font = pygame.font.Font('freesansbold.ttf', 40)
 timer = pygame.time.Clock()
 fps = 60
+
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill('dark gray')
+
+    for event in pygame.event.get():
+        
